0c85bd14f3c1dc83a3f64c864cb11d7352f1d2f0
ludivinefafournoux <ludivine.fafournoux@hotmail.fr>
2017-02-20 11:31:42 +0100

    Base de l’application
    Base de l’application

---
+++ public/service-worker.js
@@ -0,0 +1,28 @@
+var cacheName = 'cvPWA-v-1-1';
+var filesToCache = [
+  '/',
+  '/index.html',
+  '/scripts/app.js',
+  '/styles/inline.css',
+  '/images/clear.png',
+  '/images/ic_refresh_white_24px.svg'
+];
+
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(e) {
+  console.log('[ServiceWorker] Fetch', e.request.url);
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
